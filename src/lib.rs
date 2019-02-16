1c52d78cd6fb21ccd9ce7738340576d8a423b372
knoby <11472032+knoby@users.noreply.github.com>
2019-02-16 09:37:41 +0100

    Implementing CRC Calculation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,38 @@
 #![no_std]
+
+/// Calculate the CRC value for a given array of bytes
+/// The function uses CRC-8-CIIT
+pub fn calc_crc8(data: &[u8]) -> u8 {
+    // The CRC Mask
+    let mask = 0x07;
+
+    let mut crc = 0x00;
+
+    for byte in data {
+        crc ^= byte;
+
+        for _ in 0..8 {
+            if (crc & 0x80) >= 1 {
+                crc = (crc << 1) ^ mask;
+            } else {
+                crc <<= 1;
+            }
+        }
+    }
+
+    crc
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn crc() {
+        let data = [0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39];
+
+        let crc_value = calc_crc8(&data);
+
+        assert_eq!(crc_value, 0xF4);
+    }
+}
